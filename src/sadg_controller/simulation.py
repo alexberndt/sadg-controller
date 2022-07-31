fca1884e754732a4fdb383a1005b44ae86cace61
Alex Berndt <berndtae@gmail.com>
2022-07-31 14:20:07 +0200

    First working roadmap plot

--- src/sadg_controller/simulation.py
+++ src/sadg_controller/simulation.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
 import matplotlib.pyplot as plt
+import networkx as nx
 import numpy as np
 import rospy
 
 from sadg_controller.canvas.agent import Agent
-
-# from sadg_controller.mapf.roadmap import Roadmap
+from sadg_controller.canvas.roadmap import create_roadmap_graph
+from sadg_controller.mapf.roadmap import Roadmap
 
 
 def simulation():
@@ -14,17 +15,26 @@ def simulation():
     rospy.init_node("simulation")
     rospy.loginfo("Starting up the simulation ...")
 
-    # roadmap_name = rospy.get_param("~roadmap_name")
-    agent_count = rospy.get_param("~agent_count")
+    roadmap_name = rospy.get_param("~roadmap_name", "test")
+    agent_count = rospy.get_param("~agent_count", 20)
 
-    # roadmap = Roadmap(roadmap_name)
+    roadmap = Roadmap(roadmap_name)
+    graph = create_roadmap_graph(roadmap)
 
     plt.ion()
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
+    fig, ax = plt.subplots()
     ax.grid(True)
-    ax.set_xlim((-20, 20))
-    ax.set_ylim((-20, 40))
+    pos = nx.get_node_attributes(graph, "pos")
+
+    options = {
+        "node_color": "black",
+        "node_size": 10,
+        "width": 2,
+    }
+    nx.draw_networkx(graph, pos=pos, with_labels=False, ax=ax, **options)
+    ax.tick_params(left=True, bottom=True, labelleft=True, labelbottom=True)
+    plt.xlabel("x [m]")
+    plt.ylabel("y [m]")
     ax.set_aspect("equal")
 
     colors = plt.cm.rainbow(np.arange(agent_count) / agent_count)
